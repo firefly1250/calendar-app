9be052647d0c28d668d968d56cc90693ccbdd3ee
haruto <ynhrt1250@gmail.com>
2024-12-24 21:56:28 +0900

    read events.json

--- README.md
+++ README.md
@@ -12,6 +12,24 @@ CLIENT_ID=
 CLIENT_SECRET=
 ```
 
+Create an `events.json` file with the events you want to add:
+
+```json
+[
+  {
+    "summary": "test event",
+    "start": {
+      "dateTime": "2024-12-25T10:00:00+09:00",
+      "timeZone": "Asia/Tokyo"
+    },
+    "end": {
+      "dateTime": "2024-12-25T11:00:00+09:00",
+      "timeZone": "Asia/Tokyo"
+    }
+  }
+]
+```
+
 ## Run
 
 ### Authenticate
--- src/addEvents.ts
+++ src/addEvents.ts
@@ -1,5 +1,6 @@
 import { google, calendar_v3 } from 'googleapis';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 dotenv.config({ path: '.env' });
 dotenv.config({ path: '.token' });
@@ -12,31 +13,22 @@ const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-async function addEvent() {
+async function addEvents() {
   const calendar = google.calendar({ version: 'v3', auth: oAuth2Client });
 
-  const event: calendar_v3.Schema$Event = {
-    summary: 'Google Calendar API Test with TypeScript',
-    description: 'Adding an event using Google Calendar API',
-    start: {
-      dateTime: '2024-12-25T10:00:00+09:00',
-      timeZone: 'Asia/Tokyo',
-    },
-    end: {
-      dateTime: '2024-12-25T11:00:00+09:00',
-      timeZone: 'Asia/Tokyo',
-    },
-  };
+  const events: calendar_v3.Schema$Event[] = JSON.parse(fs.readFileSync('events.json', 'utf8'));
 
-  try {
-    const response = await calendar.events.insert({
-      calendarId: CALENDAR_ID,
-      requestBody: event,
-    });
-    console.log('Event has been added:', response.data.htmlLink);
-  } catch (error) {
-    console.error('Error adding event:', error);
+  for (const event of events) {
+    try {
+      const response = await calendar.events.insert({
+        calendarId: CALENDAR_ID,
+        requestBody: event,
+      });
+      console.log('Event has been added:', response.data.htmlLink);
+    } catch (error) {
+      console.error('Error adding event:', error);
+    }
   }
 }
 
-addEvent();
+addEvents();
